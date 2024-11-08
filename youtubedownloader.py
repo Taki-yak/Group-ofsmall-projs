a2360803dfca9a5df8292297ffa7ef58ad4d2c23
taki <takiyakhlef49@gmail.com>
2024-11-08 20:49:07 +0100

    adding file of Youtube downloader in pythoh

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,6 +1,8 @@
 from pytube import YouTube 
 
-videolink=str(input("Link of The Youtube video :"))
+videolink=str(input("Youtube Video Link:"))
 yt = YouTube(videolink)
-yd=yt.streams.get_highest_resolution()
-yd.download(r"your path to save the video")
+print("Title:",yt.title)
+print("views:",yt.views)
+downloader=yt.streams.get_highest_resolution()
+downloader.download(r"your-path-to-save-the-video")
