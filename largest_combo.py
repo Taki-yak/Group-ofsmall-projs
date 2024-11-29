32039538de09955748357e013381f997f567dca0
taki <takiman115@gmail.com>
2024-11-29 22:29:35 +0100

    basically no change

--- largest_combo.py
+++ largest_combo.py
@@ -15,23 +15,3 @@ class solution ():
 number=list(input(" Number:"))
 result=solution()
 print(f"The Normal ordered number :{result.large(number)}")
-# class Solution:
-#     def large(self, x):
-#         """Responsible for ordering the numbers in descending order."""
-#         res_temp = []
-        
-#         # Convert the input to a list of individual digits
-#         for digit in x:
-#             res_temp.append(digit)
-            
-#         # Sort the list in descending order
-#         for i in range(len(res_temp)):
-#             for j in range(i + 1, len(res_temp)):
-#                 if res_temp[i] < res_temp[j]:
-#                     res_temp[i], res_temp[j] = res_temp[j], res_temp[i]  # Swap
-        
-#         return ''.join(res_temp)  # Join the sorted list into a string
-
-# number = input("Number: ")
-# result = Solution()
-# print(f"The Normal ordered number: {result.large(number)}")
